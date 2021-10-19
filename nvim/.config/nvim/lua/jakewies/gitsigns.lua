00912f673870aa395f49ac9b45210400ed571ffb
Jake Wiesler <jakewiesler@gmail.com>
2021-10-18 21:47:23 -0400

    update gitsigns config

--- nvim/.config/nvim/lua/jakewies/gitsigns.lua
+++ nvim/.config/nvim/lua/jakewies/gitsigns.lua
@@ -1,9 +1,15 @@
 require('gitsigns').setup {
   signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
+    -- These hl colors are specific to gruvbox.
+    -- Switching themes will cause sign colors to break.
+    add          = {hl = 'Green'   , text = ' │'},
+    change       = {hl = 'Blue', text = ' │'},
+    delete       = {hl = 'Red', text = ' _'},
+    topdelete    = {hl = 'Red', text = ' ‾'},
+    changedelete = {hl = 'Red', text = ' ~'},
   },
+  current_line_blame = true,
+  current_line_blame_opts = {
+      virt_text_pos = 'right_align'
+  }
 }
