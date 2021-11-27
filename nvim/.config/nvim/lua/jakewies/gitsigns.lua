2b4022c62a7752e053fc033c1de45be162afc5c1
Jake Wiesler <jakewiesler@gmail.com>
2021-11-27 15:57:57 -0500

    remove gruvbox-specific gitsigns colors

--- nvim/.config/nvim/lua/jakewies/gitsigns.lua
+++ nvim/.config/nvim/lua/jakewies/gitsigns.lua
@@ -1,13 +1,4 @@
 require('gitsigns').setup {
-  signs = {
-    -- These hl colors are specific to gruvbox.
-    -- Switching themes will cause sign colors to break.
-    add          = {hl = 'Green'   , text = ' │'},
-    change       = {hl = 'Blue', text = ' │'},
-    delete       = {hl = 'Red', text = ' _'},
-    topdelete    = {hl = 'Red', text = ' ‾'},
-    changedelete = {hl = 'Red', text = ' ~'},
-  },
   current_line_blame = true,
   current_line_blame_opts = {
       virt_text_pos = 'right_align'
