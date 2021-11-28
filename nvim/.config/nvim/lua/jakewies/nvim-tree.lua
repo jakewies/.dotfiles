2b98b6c0e33442e5a71276cfd7cf5893cc39b471
Jake Wiesler <jakewiesler@gmail.com>
2021-11-28 09:47:09 -0500

    update nvim-tree config

--- nvim/.config/nvim/lua/jakewies/nvim-tree.lua
+++ nvim/.config/nvim/lua/jakewies/nvim-tree.lua
@@ -1,1 +1,8 @@
-require('nvim-tree').setup {}
+-- must be set before calling require
+vim.g.nvim_tree_indent_markers = 1 -- this option shows indent markers when folders are open
+
+require('nvim-tree').setup {
+    view = {
+        hide_root_folder = true
+    }
+}
