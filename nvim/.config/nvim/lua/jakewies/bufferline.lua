02201e9d4f72bba82b6cd9f037e07f825188d198
Jake Wiesler <jakewiesler@gmail.com>
2021-11-29 16:33:28 -0500

    update bufferline offset color based on nvim-tree

--- nvim/.config/nvim/lua/jakewies/bufferline.lua
+++ nvim/.config/nvim/lua/jakewies/bufferline.lua
@@ -3,7 +3,12 @@ require('bufferline').setup {
         diagnostics = false,
         modified_icon = '~',
         offsets = {
-            {filetype = "NvimTree", text = "File Explorer", text_align = "center"}
+            {
+                filetype = "NvimTree", 
+                text = "File Explorer",
+                text_align = "center",
+                highlight = "NvimTreeNormal"
+            }
         },
         show_buffer_icons = false,
         show_buffer_close_icons = false,
