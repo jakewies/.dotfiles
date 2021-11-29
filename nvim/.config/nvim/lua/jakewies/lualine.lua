8c9083f894e6a5552c387457e11043245d1166a4
Jake Wiesler <jakewiesler@gmail.com>
2021-11-29 16:54:22 -0500

    remove buffers from lualine

--- nvim/.config/nvim/lua/jakewies/lualine.lua
+++ nvim/.config/nvim/lua/jakewies/lualine.lua
@@ -1,10 +1,13 @@
 require('lualine').setup {
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', {'diagnostics', sources={'nvim_lsp'}}},
-    lualine_c = {},
-    lualine_x = {'buffers'},
-    lualine_y = {'location'},
-    lualine_z = {'encoding'}
-  }
+    options = {
+        disabled_filetypes = {'NvimTree'}
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', {'diagnostics', sources={'nvim_lsp'}}},
+        lualine_c = {},
+        lualine_x = {'filetype'},
+        lualine_y = {'location'},
+        lualine_z = {'encoding'}
+    }
 }
