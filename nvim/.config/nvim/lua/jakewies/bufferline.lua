1f11e2d3b6280c26bfd472824a802093329fe781
Jake Wiesler <jakewiesler@gmail.com>
2021-11-29 16:37:12 -0500

    add highlight overrides to bufferline

--- nvim/.config/nvim/lua/jakewies/bufferline.lua
+++ nvim/.config/nvim/lua/jakewies/bufferline.lua
@@ -16,5 +16,36 @@ require('bufferline').setup {
         enforce_regular_tabs = false,
         -- hide separators
         separator_style = {'',''},
+    },
+    highlights = {
+        fill = {
+            guifg = {
+                attribute = 'fg',
+                highlight = 'NvimTreeNormal'
+            },
+            guibg = {
+                attribute = 'bg',
+                highlight = 'NvimTreeNormal'
+            }
+        },
+        -- style of unselected buffers
+        background = {
+            guibg = {
+                attribute = 'bg',
+                highlight = 'NvimTreeNormal'
+            }
+        },
+        -- selected buffer, but not focused. Ex: nvim-tree has focus
+        -- a little wonky when two+ buffers are selected (splits)
+        buffer_visible = {
+            guibg = {
+                attribute = 'bg',
+                highlight = 'BufferLineBufferSelected'
+            }
+        },
+        -- remove text styles from selected buffer
+        buffer_selected = {
+            gui = "none"
+        },
     }
 }
