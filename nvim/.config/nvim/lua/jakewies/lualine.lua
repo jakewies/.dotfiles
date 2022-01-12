06db16271ecaccbe44c5c715f1a68334e01f1c7b
Jake Wiesler <jakewiesler@gmail.com>
2022-01-12 11:31:50 -0500

    refactor nvim_lsp to nvim_diagnostic

--- nvim/.config/nvim/lua/jakewies/lualine.lua
+++ nvim/.config/nvim/lua/jakewies/lualine.lua
@@ -4,7 +4,7 @@ require('lualine').setup {
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', {'diagnostics', sources={'nvim_lsp'}}},
+        lualine_b = {'branch', 'diff', {'diagnostics', sources={'nvim_diagnostic'}}},
         lualine_c = {},
         lualine_x = {'filetype'},
         lualine_y = {'location'},
